005baf2e0c10ebe39eeb951a35ab3b2da693af13
otsairaze <ssavelevdenchikk@gmail.com>
2025-08-15 21:23:48 +0300

    feat(authentication-service): add auth_users database schema

---
+++ authentication-service/database/001_create_auth_users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE auth_users (
+                            id SERIAL PRIMARY KEY,
+                            username TEXT UNIQUE NOT NULL,
+                            password_hash TEXT NOT NULL,
+                            telegram_id TEXT UNIQUE,
+                            phone TEXT UNIQUE,
+                            email TEXT UNIQUE,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
