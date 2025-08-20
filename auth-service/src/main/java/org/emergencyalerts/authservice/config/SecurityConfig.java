e6af10e06849c33828fa2f65bce520cc98c3a185
otsairaze <ssavelevdenchikk@gmail.com>
2025-08-20 18:19:53 +0300

    feat(authservice): add spring config, create auth routes

---
+++ auth-service/src/main/java/org/emergencyalerts/authservice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.emergencyalerts.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configure(http))
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/register", "/auth/login").permitAll().requestMatchers("/auth/**").authenticated()
+                .anyRequest().authenticated()
+        );
+        return http.build();
+    }
+}
